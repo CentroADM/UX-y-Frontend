70752037c6baec3b6f0396e14fc721a50321e01c
furenku <furenku@gmail.com>
2017-11-15 22:04:59 -0600

    cambiar nombre clase 3 y 4

---
+++ clase3y4/js/intro.js
@@ -0,0 +1,14 @@
+
+// ejemplo de JS tradicional:
+
+contenido = document.getElementById( "contenido" )
+
+contenido.style.color = "red"
+
+contenido.onclick = function(){
+
+  contenido.style.color = "blue"
+
+}
+
+console.log( "JS listo" )
